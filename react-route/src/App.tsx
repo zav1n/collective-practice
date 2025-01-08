de35afc62a5934b6521a437e4bc103f3cf13fe3a
zaven <2avvv1n@gmail.com>
2025-01-08 16:12:29 +0800

    feat: 首页

--- react-route/src/App.tsx
+++ react-route/src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Link } from "react-router-dom";
 import Home from "./page/Home.tsx";
 import About from "./page/About.tsx";
 import Warp from "./page/Warp.tsx";
@@ -12,22 +12,31 @@ import LayoutContain from "./page/PageLayout/LayoutContain.tsx";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Warp />}>
-        <Route index element={<WarpOne />} />
-        <Route path="teams" element={<Teams />}>
-          <Route path=":teamId" element={<TeamsId />}></Route>
-          <Route path=":teamId/edit" element={<TeamsIdEdit />}></Route>
-          <Route index element={<TeamsSetting />}></Route>
-        </Route>
-      </Route>
-      <Route path="/" element={<Home />}></Route>
+    <>
+      <div className="nav">
+        <Link to="/">首页</Link>
+        <Link to="/Warp">Warp首页</Link>
+        <Link to="/layoutContain">首页</Link>
+        <Link to="/about">关于</Link>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
 
-      <Route element={<PageLayout />}>
-        <Route path="/layoutContain" element={<LayoutContain />}></Route>
-      </Route>
-      <Route path="/about" element={<About />}></Route>
-    </Routes>
+        <Route path="/Warp" element={<Warp />}>
+          <Route index element={<WarpOne />} />
+          <Route path="teams" element={<Teams />}>
+            <Route path=":teamId" element={<TeamsId />}></Route>
+            <Route path=":teamId/edit" element={<TeamsIdEdit />}></Route>
+            <Route index element={<TeamsSetting />}></Route>
+          </Route>
+        </Route>
+
+        <Route element={<PageLayout />}>
+          <Route path="/layoutContain" element={<LayoutContain />}></Route>
+        </Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
+    </>
   );
 }
 
